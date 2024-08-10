559e072d58a02baf9cc6f91a7b76535f5f82f4ad
Fire-Horse27 <firehorse2727@gmail.com>
2024-08-09 23:36:36 -0500

    Initial Commit

---
+++ src/main/java/net/fire_horse27/morewood/MoreWoodClient.java
@@ -0,0 +1,10 @@
+package net.fire_horse27.morewood;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MoreWoodClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/fire_horse27/morewood/MoreWoodDataGenerator.java
@@ -0,0 +1,17 @@
+package net.fire_horse27.morewood;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+import net.fire_horse27.morewood.datagen.*;
+
+public class MoreWoodDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+		FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
+		pack.addProvider(ModBlockTagProvider::new);
+		pack.addProvider(ModItemTagProvider::new);
+		pack.addProvider(ModLootTableProvider::new);
+		pack.addProvider(ModModelProvider::new);
+		pack.addProvider(ModRecipeProvider::new);
+	}
+}
